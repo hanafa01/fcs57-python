c9c02b6941168bad61d47561a9d3bd425cb1a2f2
Hana <hana_fakhouri@hotmail.com>
2024-10-18 17:58:29 +0300

    updated assignemt 3

--- a3/hana-fcs57-a3-ex4.py
+++ a3/hana-fcs57-a3-ex4.py
@@ -26,7 +26,6 @@ def addNewReceipt():
             receipts.append({"barcode": b, "name": getItem['name'], "quantity": q,
                         "unit_price": getItem['price'], "total_price": (q * getItem['price'])})   
             
-            print(receipts)
             print('Another Item ? (yes / no)')
             i = input().strip().lower()
 
@@ -53,8 +52,6 @@ def printReceipt(receipts):
     print("\n--- Receipt ---")
     total = 0
     for item in receipts:
-        print('hana')
-        print(item['total_price'])
         total += item['total_price']
         print(f"Item Name: {item['name']} ({item['barcode']}): {item['quantity']} x {item['unit_price']} = {item['total_price']} ")
 
@@ -74,52 +71,3 @@ def startReceipt():
         startReceipt()
 
 startReceipt()
-
-
-# def check_correctness(c):
-#     while c != '1' and c != '0':
-#         print("Not get it, please choose 1 for yes, 0 for no")
-#         c = input()
-#     return c
-
-# def check_barcode_exists(b):
-#     for i in items:
-#         if i['barcode'] == b:
-#             return True
-#     return False
-
-
-# print("Add new Receipt ? 1 for yes, 0 for no")
-# c = input()
-
-# c = check_correctness(c)
-
-# while c == '1':
-#     print('ds')
-#     #add
-#     b = input('Barcode: ')
-#     be = False
-#     while not check_barcode_exists(b):
-#         print("Barcode not exists in our dekene. Try another one or press 0 to exit")
-#         b = input('Barcode: ')
-#         if b == '0':
-#             break
-    
-
-
-#     print("Add other receipt ? 1 for yes, 0 for no")
-#     e = input()
-#     check_correctness(e)
-
-#if c != '1':
-    #print
-    
-
-
-
-
-
-
-
-
-
